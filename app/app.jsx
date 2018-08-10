4830e156546de9c8b49522b4dcdbb58f70e884c1
rkm-dev <k.m_rahul@yahoo.com>
2018-08-10 18:00:39 +0530

    Add TodoApp component

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 //load foundation
 require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
@@ -10,6 +12,6 @@ $(document).foundation();
 require('style!css!sass!appStyles');
 
 ReactDOM.render(
-   <p>Bolierplate for react project</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
