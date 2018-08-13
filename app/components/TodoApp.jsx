58809088d108b09e208df65ad6de556b8693a804
rkm-dev <k.m_rahul@yahoo.com>
2018-08-13 11:55:57 +0530

    Complete handleTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,4 +1,5 @@
 var React    = require('react');
+var uuid     = require('node-uuid');
 var TodoList = require('TodoList');
 var AddTodo  = require('AddTodo');
 var TodoSearch = require('TodoSearch');
@@ -10,17 +11,25 @@ var TodoApp = React.createClass({
 			searchText: '',
 			todos: [
 				{
-					id: 1,
+					id: uuid(),
 					text: 'Walk the dog'
 				},{
-					id: 2,
+					id: uuid(),
 					text: 'Clean the yard'
 				}
 			]
 		};
 	},
 	handleAddTodo: function (text) {
-		alert("new todo: " + text);
+		this.setState({
+			todos: [
+				...this.state.todos,
+				{
+					id: uuid(),
+					text: text
+				}
+			]
+		});
 	},
 	handleSearch: function (showCompleted, searchText) {
 		this.setState({
